d84900b8b817399997674f2e732e498e2402fd6e
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-04-21 22:45:06 +1000

    menubar: added padding on the x-axis, texts are now not selectable and the mouse cursos will always stay as default

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function MenuBar() {
     return (
-        <div className="w-screen flex flex-row flex-nowrap justify-between items-center bg-black text-gray-50">
+        <div className="w-screen px-5 flex flex-row flex-nowrap justify-between items-center bg-black text-gray-50 cursor-default select-none">
             <div>1</div>
             <div>2</div>
             <div>3</div>
