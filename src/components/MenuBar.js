b5bfe17aab3495782cf98a07a087fdac5ec275c8
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-04-21 22:20:02 +1000

    used tailwindcss to style the menubar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function MenuBar() {
     return (
-        <div className="w-screen bg-black text-gray-50">
-            Test
+        <div className="w-screen flex flex-row flex-nowrap justify-between items-center bg-black text-gray-50">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
         </div>
     );
 }
