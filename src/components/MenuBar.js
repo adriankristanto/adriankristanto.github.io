05cfac1488f1d4a6f91531e44c5ab1c31e16fcdb
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-04-24 01:02:41 +1000

    added Clock component and put it in the MenuBar component

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,13 +1,12 @@
 import React from "react";
+import Clock from "./Clock";
 
 function MenuBar() {
     return (
         <div className="w-screen px-5 flex flex-row flex-nowrap justify-between items-center bg-black text-gray-50 cursor-default select-none">
             <div>1</div>
-            <div>2</div>
+            <div><Clock /></div>
             <div>3</div>
-            <div>4</div>
-            <div>5</div>
         </div>
     );
 }
