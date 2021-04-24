551f41ba62c5851dd0f86ccd9226f0b6448886b7
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-04-24 18:02:46 +1000

    when the control centre is clicked, it now will modify its visibility. the first click will trigger onFocus (onClick will only change focus state, but not the visibility of the control centre). subsequent click will change the visibility of the control centre.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,7 +3,10 @@ import Clock from "./Clock";
 import ControlCentre from "./ControlCentre";
 
 function MenuBar() {
-    const [controlCentre, setControlCentre] = useState(false);
+    const [controlCentre, setControlCentre] = useState({
+        visible: false,
+        focus: false
+    });
 
     const menuBarItemClassName = "px-3 py-1 rounded focus:outline-none focus:bg-gray-500";
 
@@ -20,9 +23,15 @@ function MenuBar() {
                 </div>
 
                 {/* Control Centre */}
-                <div tabIndex="0" className={menuBarItemClassName} onFocus={() => setControlCentre(true)} onBlur={() => setControlCentre(false)}>
+                <div tabIndex="0" className={menuBarItemClassName} 
+                    onFocus={() => setControlCentre({visible: true, focus: true})} 
+                    onBlur={() => setControlCentre({visible: false, focus:false})}
+                    // the first click will trigger onFocus to open the control centre -> don't trigger onClick to close it immediately
+                    // subsequent click after onFocus will modify the visibility of the control centre
+                    onClick={() => controlCentre.focus ? setControlCentre({visible: controlCentre.visible, focus: false}) : setControlCentre({visible: !controlCentre.visible, focus: false})}
+                    >
                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 6H7c-3.31 0-6 2.69-6 6s2.69 6 6 6h10c3.31 0 6-2.69 6-6s-2.69-6-6-6zm0 10H7c-2.21 0-4-1.79-4-4s1.79-4 4-4h10c2.21 0 4 1.79 4 4s-1.79 4-4 4zm0-7c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>
-                    <ControlCentre visible={controlCentre}/>
+                    <ControlCentre visible={controlCentre.visible}/>
                 </div>
 
                 <div tabIndex="0" className={menuBarItemClassName}>
