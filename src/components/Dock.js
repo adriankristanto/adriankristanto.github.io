c4885da047f9563745afb72c2e05f0d7155f62f7
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-04-27 03:22:24 +1000

    added shadow to the dock

--- src/components/Dock.js
+++ src/components/Dock.js
@@ -5,7 +5,7 @@ function Dock(props) {
 
     return (
         <div className="absolute bottom-0.5  w-screen flex justify-center">
-            <div className="flex mx-0.5 w-auto z-50 px-1.5 py-3 bg-gray-300 text-black dark:bg-black dark:text-gray-50 rounded-2xl">
+            <div className="flex mx-0.5 w-auto z-50 px-1.5 py-3 bg-gray-300 text-black dark:bg-black dark:text-gray-50 rounded-2xl shadow-lg">
 
                 <DockItem id="video-player" openApp={e => props.handleAppIconClick(e.target.id)} icon="VP"/>
                 <DockItem id="terminal" openApp={e => props.handleAppIconClick(e.target.id)} icon="T"/>
