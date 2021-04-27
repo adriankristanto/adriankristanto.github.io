a4c223adc4138fc7a26732c5e7cf6b4b59054883
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-04-28 01:58:38 +1000

    added the first app: video player

---
+++ src/components/apps/VideoPlayer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function VideoPlayer() {
+    return (
+        <iframe className="rounded-b-lg w-full h-full" src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1" title="YouTube video player" frameborder="0"></iframe>
+    )
+}
+
+export default VideoPlayer;
